adc602126e739cedd7fb362052acdfd8e8013ff0
Helder Pereira <helder_leg@hotmail.com>
2024-03-16 15:31:31 -0300

    Notas das aulas de JavaScripts

---
+++ main.js
@@ -0,0 +1,44 @@
+// Strings
+
+// ex01
+// const message = 'Hello World'
+
+// console.log(message.length)
+
+// ex02
+// const firstName = 'Helder'
+// const lastName = 'Pereira'
+
+// // forma antiga de exibir textos no console
+// console.log('Meu nome é' + ' ' + firstName + ' ' + lastName)
+// // forma atual de exibir textos no console
+// console.log(`Meu nome é ${firstName} ${lastName}`)
+
+
+// ex03
+// fatiando strings
+// const names ='Helder,Victor,João'
+// console.log(names.split(","))
+
+
+// Numeros
+
+// const number = 5
+
+// console.log(number.toString())
+// console.log(typeof number)
+
+
+// Boleanos
+// false
+// true
+
+// console.log(2 == 3)
+
+// Null e Undefined
+
+// const x = null
+
+// const y = undefined 
+
+// const list = [1,2,3,4,5,6]
